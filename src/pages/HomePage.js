16780e9e65c8f513442dda31f57477cbd5687c1c
rahulkottikasala <rahulkottikasala@gmail.com>
2022-01-29 05:29:59 +0400

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,37 @@
-import React,{useState} from 'react';
-import AddNew from '../component/AddNew';
-import TodoList from '../list/TodoList';
-import './HomePage.css'
+import React, { useState } from "react";
+import AddNew from "../component/AddNew";
+import TodoList from "../list/TodoList";
+import "./HomePage.css";
 
 const HomePage = () => {
+  const [todos, setTodos] = useState([]);
 
-    const [todos,setTodos] = useState([])
+  const handleTodoDelete = (id) => {
+    const newArray = todos.filter((element) => element.id !== id);
+    setTodos(newArray);
+  };
+  const handleTodoStatus = (status, id) => {
+    setTodos(
+      todos.filter((obj) => {
+        if (obj.id === id) {
+          obj.status = status;
+        }
+        return obj;
+      })
+    );
+  };
 
-    
-
-console.log(todos);
-  return <div className="home-page">
-     <AddNew setTodos={setTodos} todos={todos} />
-   <TodoList setTodos={setTodos} todos={todos} />
-  </div>;
+  console.log(todos);
+  return (
+    <div className="home-page">
+      <AddNew setTodos={setTodos} todos={todos} />
+      <TodoList
+        todos={todos}
+        handleTodoDelete={handleTodoDelete}
+        handleTodoStatus={handleTodoStatus}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
